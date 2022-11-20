67f071fd0af4afbaed8a9660f65846f6e66aded6
Nahuel <nlcostamagna@gmail.com>
2022-11-20 01:38:23 -0300

    initial project

---
+++ cmd/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/ncostamagna/gocourse_enrollment/internal/enrollment"
+	"github.com/ncostamagna/gocourse_enrollment/pkg/bootstrap"
+	"github.com/ncostamagna/gocourse_enrollment/pkg/handler"
+)
+
+func main() {
+
+	_ = godotenv.Load()
+	l := bootstrap.InitLogger()
+	db, err := bootstrap.DBConnection()
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	pagLimDef := os.Getenv("PAGINATOR_LIMIT_DEFAULT")
+	if pagLimDef == "" {
+		l.Fatal("paginator limit default is required")
+	}
+
+	ctx := context.Background()
+	enrollRepo := enrollment.NewRepo(db, l)
+	enrollSrv := enrollment.NewService(l, enrollRepo)
+	h := handler.NewEnrollmentHTTPServer(ctx, enrollment.MakeEndpoints(enrollSrv, enrollment.Config{LimPageDef: pagLimDef}))
+	port := os.Getenv("PORT")
+	address := fmt.Sprintf("127.0.0.1:%s", port)
+	srv := &http.Server{
+		Handler:      accessControl(h),
+		Addr:         address,
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  4 * time.Second,
+	}
+
+	errCh := make(chan error)
+
+	go func() {
+		l.Println("listen in ", address)
+		errCh <- srv.ListenAndServe()
+	}()
+
+	err = <-errCh
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func accessControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, HEAD, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
---
+++ internal/enrollment/repository.go
@@ -0,0 +1,103 @@
+package enrollment
+
+import (
+	"context"
+	"log"
+
+	"github.com/ncostamagna/gocourse_domain/domain"
+	"gorm.io/gorm"
+)
+
+type (
+	Repository interface {
+		Create(ctx context.Context, enroll *domain.Enrollment) error
+		GetAll(ctx context.Context, filters Filters, offset, limit int) ([]domain.Enrollment, error)
+		Update(ctx context.Context, id string, status *string) error
+		Count(ctx context.Context, filters Filters) (int, error)
+	}
+
+	repo struct {
+		db  *gorm.DB
+		log *log.Logger
+	}
+)
+
+//NewRepo is a repositories handler
+func NewRepo(db *gorm.DB, l *log.Logger) Repository {
+	return &repo{
+		db:  db,
+		log: l,
+	}
+}
+
+func (r *repo) Create(ctx context.Context, enroll *domain.Enrollment) error {
+
+	if err := r.db.WithContext(ctx).Create(enroll).Error; err != nil {
+		r.log.Println(err)
+		return err
+	}
+	r.log.Println("enrollment created with id: ", enroll.ID)
+	return nil
+}
+
+func (r *repo) GetAll(ctx context.Context, filters Filters, offset, limit int) ([]domain.Enrollment, error) {
+	var e []domain.Enrollment
+
+	tx := r.db.WithContext(ctx).Model(&e)
+	tx = applyFilters(tx, filters)
+	tx = tx.Limit(limit).Offset(offset)
+	result := tx.Order("created_at desc").Find(&e)
+
+	if result.Error != nil {
+		r.log.Println(result.Error)
+		return nil, result.Error
+	}
+	return e, nil
+}
+
+func (r *repo) Update(ctx context.Context, id string, status *string) error {
+
+	values := make(map[string]interface{})
+
+	if status != nil {
+		values["status"] = *status
+	}
+
+	result := r.db.WithContext(ctx).Model(&domain.Enrollment{}).Where("id = ?", id).Updates(values)
+	if result.Error != nil {
+		r.log.Println(result.Error)
+		return result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		r.log.Printf("enrollment %s doesn't exists", id)
+		return ErrNotFound{id}
+	}
+
+	return nil
+}
+
+func (r *repo) Count(ctx context.Context, filters Filters) (int, error) {
+	var count int64
+	tx := r.db.WithContext(ctx).Model(domain.Enrollment{})
+	tx = applyFilters(tx, filters)
+	if err := tx.Count(&count).Error; err != nil {
+		r.log.Println(err)
+		return 0, err
+	}
+
+	return int(count), nil
+}
+
+func applyFilters(tx *gorm.DB, filters Filters) *gorm.DB {
+
+	if filters.UserID != "" {
+		tx = tx.Where("user_id = ?", filters.UserID)
+	}
+
+	if filters.CourseID != "" {
+		tx = tx.Where("course_id = ?", filters.CourseID)
+	}
+
+	return tx
+}
