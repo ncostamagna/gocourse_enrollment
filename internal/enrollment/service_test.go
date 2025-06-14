d4fb311e25dc52bc0691d198a1f94a29263fa2fc
Nahue <nlcostamagna@gmail.com>
2025-06-13 23:46:27 -0300

    get all success

--- internal/enrollment/service_test.go
+++ internal/enrollment/service_test.go
@@ -19,8 +19,11 @@ func TestService_GetAll(t *testing.T) {
 	t.Run("should return an error", func(t *testing.T) {
 
 		var want error = errors.New("my expected error")
+		var wantCounter int = 1
+		var counter int = 0
 		repo := &RepositoryMock{
 			GetAllFunc: func(ctx context.Context, filters enrollment.Filters, offset, limit int) ([]domain.Enrollment, error) {
+				counter++
 				return nil, want
 			},
 		}
@@ -30,6 +33,33 @@ func TestService_GetAll(t *testing.T) {
 		enrollments, err := service.GetAll(context.Background(), enrollment.Filters{}, 0, 10)
 		assert.ErrorIs(t, err, want)
 		assert.Nil(t, enrollments)
+		assert.Equal(t, wantCounter, counter)
+	})
+
+	t.Run("should return all enrollments", func(t *testing.T) {
+		want := []domain.Enrollment{
+			{
+				ID: "1",
+				UserID: "1",
+				CourseID: "1",
+				Status: "active",
+			},
+		}
+		var wantCounter int = 1
+		var counter int = 0
+		repo := &RepositoryMock{
+			GetAllFunc: func(ctx context.Context, filters enrollment.Filters, offset, limit int) ([]domain.Enrollment, error) {
+				counter++
+				return want, nil
+			},
+		}
+
+		service := enrollment.NewService(l, nil, nil, repo)
+
+		enrollments, err := service.GetAll(context.Background(), enrollment.Filters{}, 0, 10)
+		assert.NoError(t, err)
+		assert.Equal(t, want, enrollments)
+		assert.Equal(t, wantCounter, counter)
 	})
 	
 	
